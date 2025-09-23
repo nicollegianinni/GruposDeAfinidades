1f326b2ef1be3e9133d4c4318f24bb69f5592722
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-09-23 13:29:00 -0300

    criado projeto grupo de afinidades

---
+++ afinidades/src/main/java/grupos/afinidades/AfinidadesApplication.java
@@ -0,0 +1,13 @@
+package grupos.afinidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfinidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfinidadesApplication.class, args);
+	}
+
+}
---
+++ afinidades/src/test/java/grupos/afinidades/AfinidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package grupos.afinidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfinidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
