40c06eb1e8e2980811d12472f35f33e812c57e1d
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-09-23 16:40:26 -0300

    criada classe pessoa

---
+++ afinidades/src/main/java/grupos/afinidades/model/Pessoa.java
@@ -0,0 +1,41 @@
+package grupos.afinidades.model;
+
+public class Pessoa {
+
+    private Long id;
+    private String nome;
+    private int idade;
+
+    public Pessoa(Long id, String nome, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Pessoa() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
